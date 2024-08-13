f46a8d0582648adb0b5032ab77f7bef9fdeb1c9c
cml2517103 <cml2517103@gmail.com>
2024-08-13 09:58:53 -0700

    Update Program4.java

--- Program4.java
+++ Program4.java
@@ -1,10 +1,21 @@
-//Youre on your own!
-//Jk ask for help if you need it (but also look at previous programs!!)
+import java.util.Scanner;
 
-
-
-//Paste console output below:
-/*
-
-
-*/
+class Program4 {
+    public static void main(String[] args){
+        Scanner survey = new Scanner(System.in);
+        
+        System.out.println("Enter four three-digit numbers...");
+        int num1 = survey.nextInt();
+        int num2 = survey.nextInt();
+        int num3 = survey.nextInt();
+        int num4 = survey.nextInt();
+        System.out.println(); // blanks line between input and output
+        
+        int sum = num1 + num2 + num3 + num4;
+        double avg = (double)sum / 4;
+        
+        System.out.println("The sum of the four numbers is " + sum);
+        System.out.println(); //blank line between outpots
+        System.out.println("The average of the four numbers is " + avg);
+    }
+}
